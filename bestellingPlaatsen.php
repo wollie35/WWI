39f47277d1e3d75bdf4ef25b0b0e023afbe2d48a
Ikbenjelle <Jelleboerma@gmail.com>
2018-11-14 11:33:48 +0100

    Update bestellingPlaatsen.php

--- bestellingPlaatsen.php
+++ bestellingPlaatsen.php
@@ -107,51 +107,14 @@ session_start();
                         </div>
                     </div>
                     <div class="form-group">
-                        <div class="col-md-12"><strong>Credit Card Nummer:</strong></div>
+                        <div class="col-md-12"><strong>Rekeningnummer:</strong></div>
                         <div class="col-md-12"><input type="text" class="form-control" name="car_number" value="" /></div>
                     </div>
                     <div class="form-group">
-                        <div class="col-md-12"><strong>CVV code:</strong></div>
+                        <div class="col-md-12"><strong>Pas nummer:</strong></div>
                         <div class="col-md-12"><input type="text" class="form-control" name="car_code" value="" /></div>
                     </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <strong>Vervaldatum</strong>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <select class="form-control" name="">
-                                <option value="">Maand</option>
-                                <option value="01">01</option>
-                                <option value="02">02</option>
-                                <option value="03">03</option>
-                                <option value="04">04</option>
-                                <option value="05">05</option>
-                                <option value="06">06</option>
-                                <option value="07">07</option>
-                                <option value="08">08</option>
-                                <option value="09">09</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                            </select>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <select class="form-control" name="">
-                                <option value="">Jaar</option>
-                                <option value="2015">2018</option>
-                                <option value="2016">2019</option>
-                                <option value="2017">2020</option>
-                                <option value="2018">2021</option>
-                                <option value="2019">2022</option>
-                                <option value="2020">2023</option>
-                                <option value="2021">2024</option>
-                                <option value="2022">2025</option>
-                                <option value="2023">2026</option>
-                                <option value="2024">2027</option>
-                                <option value="2025">2028</option>
-                            </select>
-                        </div>
-                    </div>
+
 
                 </div>
                 <div class="form-group">
