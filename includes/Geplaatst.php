9e1439e36661d8cb3bce7617aaf1603a9a8c5159
Rosa <s1131954@student.windesheim.nl>
2018-11-14 10:43:30 +0100

    Betaalding

---
+++ includes/Geplaatst.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Uw betaling is gelukt en de bestelling is geplaats. <br>
+       Bedankt voor het winkelen bij Wide World Importers!
+
+    </body>
+</html>
+
+
