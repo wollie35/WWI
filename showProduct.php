40d64b2d5311f7d388107cbfc36265336223c213
Wolter van Donk <woltervandonk@gmail.com>
2018-11-16 13:56:10 +0100

    button change

--- showProduct.php
+++ showProduct.php
@@ -74,7 +74,7 @@ session_start();
                         <th>Opmerking leverancier</th>
                         <th>Rating</th>
                         <th> <form method="POST">
-                                <input type="submit"  name="addToCart" dirname="" value="<?= $selectedProduct[$x][0] ?>"  />
+                                <input type="submit" class="btn btn-info" name="addToCart" dirname="" value="<?= $selectedProduct[$x][0] ?>"  />
                                 <form></th>
                                     </tr>
                                     <tr>
