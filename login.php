6a3f1a7de6585c2baf95e9a9820fd5436a8f7e45
Marie-Claire <marieclaire1998@hotmail.com>
2018-11-16 12:07:43 +0100

    Update login.php

--- login.php
+++ login.php
@@ -59,7 +59,7 @@ session_start();
                                     <div class="row">
                                         <div class="col-lg-12">
                                             <div class="text-center">
-                                                <a href="https://phpoll.com/recover" tabindex="5" class="forgot-password">Wachtwoord vergeten?</a>
+                                                <a href="localhost/wwi/wwi/wachtwoordVergeten.php" tabindex="5" class="forgot-password">Wachtwoord vergeten?</a>
                                             </div>
                                         </div>
                                     </div>
