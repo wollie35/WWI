7e1ae5fd5b4e38ef60de633fd4bfb5bbc244d34c
Ikbenjelle <Jelleboerma@gmail.com>
2018-11-16 11:01:45 +0100

    Update bestellingAfronden.php

--- bestellingAfronden.php
+++ bestellingAfronden.php
@@ -52,25 +52,27 @@ unset($_SESSION["countBestelling"]);
 
                 <br>
                 Bedankt voor het winkelen bij Wide World Importers!
+                <br>
+                <form method="post" action="index.php">
+                    <input type="submit" value="Terug naar hoofdpagina" class="btn btn-success">
 
-            </div>
-            <!-- /.container -->
+                    </div>
+                    <!-- /.container -->
 
-            <!-- Footer -->
-            <footer class="py-5 bg-dark">
-                <div class="container">
-                    <p class="m-0 text-center text-white">Copyright &copy; Wide World Importers 2018</p>
-                </div>
-                <!-- /.container -->
-            </footer>
+                    <!-- Footer -->
+                    <footer class="py-5 bg-dark">
+                        <div class="container">
+                            <p class="m-0 text-center text-white">Copyright &copy; Wide World Importers 2018</p>
+                        </div>
+                        <!-- /.container -->
+                    </footer>
 
-            <!-- Bootstrap core JavaScript -->
-            <script src="vendor/jquery/jquery.min.js"></script>
-            <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                    <!-- Bootstrap core JavaScript -->
+                    <script src="vendor/jquery/jquery.min.js"></script>
+                    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                    </body>
 
-    </body>
-
-</html>
+                    </html>
 
 
 
