9d74edafe72c1298899fb9274448a06260706330
Wolter van Donk <woltervandonk@gmail.com>
2018-11-23 13:09:15 +0100

    login redirect nu naar index.php

--- login.php
+++ login.php
@@ -6,103 +6,111 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <?= displayHeader(); ?>
-        <link href="css/table.css" rel="stylesheet">
-    </head>
+<head>
+    <?= displayHeader(); ?>
+    <link href="css/table.css" rel="stylesheet">
+</head>
 
-    <body>
-        <?php
-        
-        if (isset($_POST["login"])) {
-            $db = DBconnectie();
-            $username = "'".$_POST['username']."'";
+<body>
+<?php
 
-            $query = 'SELECT id, username, password FROM users WHERE username = '.$username;
-            $sql = $db->prepare($query);
+if (isset($_POST["login"])) {
+    $db = DBconnectie();
+    $username = "'" . $_POST['username'] . "'";
+
+    $query = 'SELECT id, username, password FROM users WHERE username = ' . $username;
+    $sql = $db->prepare($query);
 //            var_dump($query);
-            $sql->execute();
-            header('Location: index.php');
+    $sql->execute();
 
-            $sql = $sql->fetchAll();
 
-            echo count($sql);
-            if (count($sql) == 1){
-            if ($sql[0][2] == $_POST['password']){
-                print("Log in succesvol");
-                $_SESSION['username'] = $_POST['username'];
+    $sql = $sql->fetchAll();
+
+    if (count($sql) == 1) {
+        if ($sql[0][2] == $_POST['password']) {
+            ?>
+            <script language="javascript" type="text/javascript">
+                <!--
+                window.setTimeout('window.open("index.php")', 0);
+                // -->
+            </script>
+            <?php
+            $_SESSION['username'] = $_POST['username'];
 //                $_SESSION['id'] = $sql[0][0];
 //                echo $_SESSION['id'];
-            } else {
-                echo displayModal("Foutmelding", "De combinatie van gebruikersnaam en e-mailadres is niet geldig", "Sluit");
-
-            }
         } else {
-                echo displayModal("Foutmelding", "De combinatie van gebruikersnaam en e-mailadres is niet geldig", "Sluit");
-            }
+            echo displayModal("Foutmelding", "De combinatie van gebruikersnaam en e-mailadres is niet geldig", "Sluit");
+
         }
-        ?>
-        <!-- Navigation -->
-        <?= displayNavBar(); ?>
+    } else {
+        echo displayModal("Foutmelding", "De combinatie van gebruikersnaam en e-mailadres is niet geldig", "Sluit");
+    }
+}
+?>
+<!-- Navigation -->
+<?= displayNavBar(); ?>
 
-        <!-- Page Content -->
-        <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-            <div class="row">
-                <div class="col-lg-3">
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+    </div>
+    <!-- /.col-lg-3 -->
+    <div class="container">
+        <div class="row centered-form" style="margin-left: 40%">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Inloggen</h3>
                 </div>
-            </div>
-            <!-- /.col-lg-3 -->
-            <div class="container" >
-                <div class="row centered-form" style="margin-left: 40%">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Inloggen</h3>
-                        </div>
-                        <div class="panel-body">
-                            <form method="post">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input type="text" required name="username" id="username" class="form-control input-sm" placeholder="Gebruikersnaam">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <input type="password" required name="password" id="password" class="form-control input-sm" placeholder="Wachtwoord">
-                                        </div>
-                                    </div>
-                                </div>
-                                <input type="submit" name="login" value="Inloggen" class="btn btn-info btn-block">
+                <div class="panel-body">
+                    <form method="post">
+                        <div class="row">
+                            <div class="col-md-12">
                                 <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="text-center">
-                                                Heb je nog geen account? Registreer dan <a href="register.php" tabindex="5" class="forgot-password">hier</a>
-                                            </div>
-                                        </div>
+                                    <input type="text" required name="username" id="username"
+                                           class="form-control input-sm" placeholder="Gebruikersnaam">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input type="password" required name="password" id="password"
+                                           class="form-control input-sm" placeholder="Wachtwoord">
+                                </div>
+                            </div>
+                        </div>
+                        <input type="submit" name="login" value="Inloggen" class="btn btn-info btn-block">
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="text-center">
+                                        Heb je nog geen account? Registreer dan <a href="register.php" tabindex="5"
+                                                                                   class="forgot-password">hier</a>
                                     </div>
                                 </div>
-                            </form>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <?php
-
-        ?>
     </div>
-    <!-- /.container -->
+</div>
+<?php
 
-    <!-- Footer -->
-        <?= displayFooter();?>
+?>
+</div>
+<!-- /.container -->
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Footer -->
+<?= displayFooter(); ?>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
